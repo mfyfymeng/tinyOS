586a3d5402418a245bc75d8b8559ba71b4ed10ff
mfyfymeng <mfyfymeng@163.com>
2019-12-19 09:16:30 +0800

    first commit

---
+++ c3.03 tinyOS/Objects/main.d
@@ -0,0 +1,11 @@
+.\objects\main.o: Source\main.c
+.\objects\main.o: Source\tinyOS.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: Source\tLib.h
+.\objects\main.o: C:\Users\fymeng\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\Device\ARM\ARMCM3\Include\ARMCM3.h
+.\objects\main.o: C:\Users\fymeng\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm3.h
+.\objects\main.o: C:\Users\fymeng\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: C:\Users\fymeng\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: C:\Users\fymeng\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: C:\Users\fymeng\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\main.o: C:\Users\fymeng\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\Device\ARM\ARMCM3\Include\system_ARMCM3.h
