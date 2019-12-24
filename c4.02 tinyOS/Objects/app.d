f75a68da75bd6f0e95d5af182f9f8bdff46127c7
mfyfymeng <mfyfymeng@163.com>
2019-12-24 18:48:22 +0800

    first commit

---
+++ c4.02 tinyOS/Objects/app.d
@@ -0,0 +1,5 @@
+.\objects\app.o: Source\app.c
+.\objects\app.o: Source\tinyOS.h
+.\objects\app.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\app.o: Source\tLib.h
+.\objects\app.o: Source\tConfig.h
